d0d1c2cd4bacff31a55d0602d8b6170058f1b643
thangnguyenvn98 <thangnguyenvn647@gmail.com>
2023-04-19 00:04:43 -0400

    Fixing deployment isseus with getListings

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,83 +1,96 @@
-import prisma from '@/app/libs/prismadb'
+import prisma from "@/app/libs/prismadb";
 
 export interface IListingsParams {
-    userId?:string;
-    guestCount?:number;
-    roomCount?:number;
-    bathroomCount?:number;
-    startDate?:string;
-    endDate?:string;
-    locationValue?:string;
-    category?:string
-
+  userId?: string;
+  guestCount?: number;
+  roomCount?: number;
+  bathroomCount?: number;
+  startDate?: string;
+  endDate?: string;
+  locationValue?: string;
+  category?: string;
 }
-export default async function getListings(params:IListingsParams){
-    try {
-        const {userId,guestCount, roomCount,bathroomCount,startDate,endDate,locationValue,category} = params
-        let query : any = {}
 
-         if(userId) {
-            query.userId = userId
-         }
+export default async function getListings(
+  params: IListingsParams
+) {
+  try {
+    const {
+      userId,
+      roomCount, 
+      guestCount, 
+      bathroomCount, 
+      locationValue,
+      startDate,
+      endDate,
+      category,
+    } = params;
 
-         if(category) {
-            query.category = category
-         }
+    let query: any = {};
 
-         if(roomCount){
-            query.roomCount = {
-                gte: +roomCount  //gte means greater than equal, making it into a number from string
-            }
-         }
-         if(guestCount){
-            query.guestCount = {
-                gte: +guestCount  //gte means greater than equal, making it into a number from string
-            }
-         }
-         if(bathroomCount){
-            query.bathroomCount = {
-                gte: +bathroomCount  //gte means greater than equal, making it into a number from string
-            }
-         }
-
-         if(locationValue) {
-            query.locationValue = locationValue
-         }
-
-         if (startDate && endDate){  //find by reversing the logic
-            query.NOT = {
-                reservations: {
-                    some : {
-                        OR : [
-                            {
-                                endDate: {gte: startDate},   //use 2 combinations to filter out conflict in reservations
-                                startDate: {lte: startDate}, //if single 
-                            },
-                            {
-                                startDate: {lte: endDate},
-                                endDate: {gte: endDate}
-                            }
-                        ]
-                    }
-                }
-            }
-         }
-         
-        const listings = await prisma.listing.findMany({
-            where: query,
-            orderBy:{
-                createdAt: 'desc'
-            }
-        })
-
-        const safeListings = listings.map((listing)=> ({
-            ...listing,
-            createdAt: listing.createdAt.toISOString(),
-            
-        }))
-
-        return safeListings
-    }catch (error:any){
-        throw new Error(error)
+    if (userId) {
+      query.userId = userId;
     }
+
+    if (category) {
+      query.category = category;
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        gte: +roomCount
+      }
+    }
+
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount
+      }
+    }
+
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount
+      }
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue;
+    }
+
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: startDate }
+              },
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate }
+              }
+            ]
+          }
+        }
+      }
+    }
+
+    const listings = await prisma.listing.findMany({
+      where: query,
+      orderBy: {
+        createdAt: 'desc'
+      }
+    });
+
+    const safeListings = listings.map((listing) => ({
+      ...listing,
+      createdAt: listing.createdAt.toISOString(),
+    }));
+
+    return safeListings;
+  } catch (error: any) {
+    throw new Error(error);
+  }
 }
