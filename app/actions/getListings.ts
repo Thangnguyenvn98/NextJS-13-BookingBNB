983067401fb468161a3e36c82725bea40bb88e56
thangnguyenvn98 <thangnguyenvn647@gmail.com>
2023-04-18 23:32:12 -0400

    Filter functionality works correctly, setting up middleware

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -2,16 +2,69 @@ import prisma from '@/app/libs/prismadb'
 
 export interface IListingsParams {
     userId?:string;
+    guestCount?:number;
+    roomCount?:number;
+    bathroomCount?:number;
+    startDate?:string;
+    endDate?:string;
+    locationValue?:string;
+    category?:string
+
 }
 export default async function getListings(params:IListingsParams){
     try {
-        const {userId} = params
+        const {userId,guestCount, roomCount,bathroomCount,startDate,endDate,locationValue,category} = params
         let query : any = {}
 
          if(userId) {
             query.userId = userId
          }
+
+         if(category) {
+            query.category = category
+         }
+
+         if(roomCount){
+            query.roomCount = {
+                gte: +roomCount  //gte means greater than equal, making it into a number from string
+            }
+         }
+         if(guestCount){
+            query.guestCount = {
+                gte: +guestCount  //gte means greater than equal, making it into a number from string
+            }
+         }
+         if(bathroomCount){
+            query.bathroomCount = {
+                gte: +bathroomCount  //gte means greater than equal, making it into a number from string
+            }
+         }
+
+         if(locationValue) {
+            query.locationValue = locationValue
+         }
+
+         if (startDate && endDate){  //find by reversing the logic
+            query.NOT = {
+                reservations: {
+                    some : {
+                        OR : [
+                            {
+                                endDate: {gte: startDate},   //use 2 combinations to filter out conflict in reservations
+                                startDate: {lte: startDate}, //if single 
+                            },
+                            {
+                                startDate: {lte: endDate},
+                                endDate: {gte: endDate}
+                            }
+                        ]
+                    }
+                }
+            }
+         }
+         
         const listings = await prisma.listing.findMany({
+            where: query,
             orderBy:{
                 createdAt: 'desc'
             }
